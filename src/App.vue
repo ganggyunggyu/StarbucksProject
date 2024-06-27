2d8087235d4e4eaa5446655e3b59aad101160d75
ganggyunggyu <qwzx16@naver.com>
2024-06-27 18:58:37 +0900

    routing & setVh setVw

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import { onMounted } from 'vue';
+  import { setVh, setVw } from './shared/lib/setViewPort';
+
+  onMounted(() => {
+    setVh();
+    setVw();
+  });
+</script>
+
+<template>
+  <router-view />
+</template>
+
+<style scoped></style>
