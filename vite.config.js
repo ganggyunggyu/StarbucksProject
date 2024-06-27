9bf09d795a400b6fef4a41904c39db9dcfa99f87
ganggyunggyu <qwzx16@naver.com>
2024-06-27 18:54:12 +0900

    create: vue project

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+
+  resolve: {
+    alias: [
+      { find: '@', replacement: '/src' },
+      { find: '@views', replacement: '/src/views' },
+      { find: '@shared', replacement: '/src/shared' },
+      { find: '@resource', replacement: '/src/static/resource' },
+    ],
+  },
+});
