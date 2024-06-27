efe39674b07e7d18c1280b388d19acc238bd7d99
ganggyunggyu <qwzx16@naver.com>
2024-06-27 18:54:37 +0900

    add: constant

---
+++ src/constant/IMAGE_URL.js
@@ -0,0 +1,5 @@
+export const PROFILE = new URL('../static/resource/event_profile.png', import.meta.url).href;
+
+export const BACKGROUND_IMAGE = new URL('../static/resource/landing_background.png', import.meta.url).href;
+
+export const EVENT_MAIN = new URL('../static/resource/event_main.png', import.meta.url).href;
---
+++ src/constant/TEXT_LIST.js
@@ -0,0 +1,11 @@
+export const LANDING_TEXT_LIST = ['베어리 스타와 함께 즐기는', '나만의 AR 만들기'];
+
+export const BUTTON_TEXT_LIST = ['바로 시작하기', '당첨 내역 확인'];
+
+export const TEXT_LIST = ['베어리스타와 함께 춤을!', '베어리스타 댄스를 함께 추고', '챌린지에도 참여해 봐! ♬'];
+export const CONTENT_LIST = ['베어리스타', '댄스 챌린지'];
+export const PERIOD = '기간 : 3/14(목) ~ 4/10(수)';
+
+export const HELPER_TEXT_LIST = ['카메라로 바닥이나 테이블을 비추면', '춤추는 베어리스타가 등장할 거야!', '챌린지도 함께 찍어보는 건 어때~?!'];
+
+export const CODE_MODAL_TEXT_LIST = ['영수증 하단에서 발급받은', '입장 코드를 입력해 주세요.'];
