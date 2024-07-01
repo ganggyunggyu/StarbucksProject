11ff4abd7e2e0c17db072a69fc04feb433800a1d
ganggyunggyu <qwzx16@naver.com>
2024-07-01 18:44:06 +0900

    chore: routing add

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Landing from '@views/Landing.vue';
 import Event from '@views/Event.vue';
 import Loading from '@views/Loading.vue';
+import Capture from '@views/Capture.vue';
+import Complate from '@views/Complate.vue';
 const routes = [
   { path: '/', component: Landing, name: 'Landing' },
   { path: '/event', component: Event, name: 'Event' },
   { path: '/loading', component: Loading, name: 'Loading' },
+  { path: '/capture', component: Capture, name: 'Capture' },
+  { path: '/complate', component: Complate, name: 'Complate' },
 ];
 
 const router = createRouter({
