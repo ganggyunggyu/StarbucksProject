77516d8a4b50bc6512479d181ed635856d2c76a8
ganggyunggyu <qwzx16@naver.com>
2024-07-01 18:45:30 +0900

    feat: 캡쳐 페이지 작업

---
+++ src/features/ui/capture/BottomNavigation.vue
@@ -0,0 +1,33 @@
+<script setup>
+  const props = defineProps(['ICONS']);
+
+  //ICONS : [name : '', component: c, label: '', onClick: f]
+</script>
+
+<template>
+  <nav>
+    <button v-for="icon in props.ICONS" :key="icon.name" @click="icon.onClick">
+      <component :is="icon.component" />
+      <p>{{ icon.label }}</p>
+    </button>
+  </nav>
+</template>
+
+<style scoped>
+  nav {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    height: calc(19 * var(--vh));
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    background-color: white;
+  }
+  nav button {
+    flex: 1;
+    line-height: 20px;
+    font-size: var(--font-size-md);
+  }
+</style>
---
+++ src/features/ui/capture/Tutorial.vue
@@ -0,0 +1,55 @@
+<script setup>
+  import { PROFILE } from '@/constant/IMAGE_URL';
+  import CloseIcon from '@/shared/ui/icon/CloseIcon.vue';
+  import { ref } from 'vue';
+  const TUTORIAL_TEXT_LIST = ['카메라를 고정시키면', '벚꽃나무가 나타날 거야!'];
+  const isTutorial = ref(true);
+</script>
+<template>
+  <div v-if="isTutorial" @click="isTutorial = false" class="tutorial">
+    <article>
+      <CloseIcon />
+      <div>
+        <img :src="PROFILE" alt="" />
+      </div>
+      <div>
+        <p v-for="p in TUTORIAL_TEXT_LIST">{{ p }}</p>
+      </div>
+    </article>
+  </div>
+</template>
+<style scoped>
+  .tutorial {
+    width: 100%;
+  }
+  .tutorial article {
+    width: 90%;
+    height: calc(16 * var(--vh));
+    background-color: white;
+    position: fixed;
+    bottom: 0;
+    z-index: 999;
+    left: 50%;
+    transform: translate(-50%, -15%);
+    border-radius: 20px;
+    display: flex;
+    align-items: center;
+    padding: 20px;
+    gap: 20px;
+  }
+  svg {
+    position: absolute;
+    top: 10px;
+    right: 5px;
+  }
+  article img {
+    width: 70px;
+    height: 70px;
+    background-color: var(--color-pink);
+    border-radius: 50%;
+  }
+  p {
+    line-height: 20px;
+    font-size: var(--font-size-md);
+  }
+</style>
