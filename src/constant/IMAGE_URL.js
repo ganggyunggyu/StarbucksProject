f631fbe81f9090c7a5847eb0cc7a1047c3998bb4
ganggyunggyu <qwzx16@naver.com>
2024-07-01 18:43:51 +0900

    chore: image add

--- src/constant/IMAGE_URL.js
+++ src/constant/IMAGE_URL.js
@@ -3,3 +3,8 @@ export const PROFILE = new URL('../static/resource/event_profile.png', import.me
 export const BACKGROUND_IMAGE = new URL('../static/resource/landing_background.png', import.meta.url).href;
 
 export const EVENT_MAIN = new URL('../static/resource/event_main.png', import.meta.url).href;
+
+export const POHTO_CARD_1 = new URL('../static/resource/photo_card_1.png', import.meta.url).href;
+export const POHTO_CARD_2 = new URL('../static/resource/photo_card_2.png', import.meta.url).href;
+
+export const CAPTURE_BACKGROUND_IMAGE = new URL('../static/resource/background_image.png', import.meta.url).href;
