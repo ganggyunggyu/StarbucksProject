18e2e94f9101c6966b0910d559736dd5f53b86fa
강경규 <qwzx16@naver.com>
2024-07-03 18:01:40 +0900

    chore: ar capture lib add

---
+++ src/shared/hooks/useExperience.js
@@ -0,0 +1,76 @@
+import Experience from '@/ar/capture/Experience';
+import router from '@/router/index';
+import { ref, onMounted, onUnmounted } from 'vue';
+import { onBeforeRouteLeave } from 'vue-router';
+
+const useExperience = () => {
+  let experience;
+
+  const setFrameSrc = ref(null);
+  const selectedCharacter = ref(false);
+  const selectedSticker = ref(false);
+  const playAudio = () => {
+    console.debug('audio');
+  };
+
+  const saveImage = (image) => {
+    let canvas = document.querySelector('.webgl');
+    let width = canvas.style.width;
+    let height = canvas.style.height;
+
+    router.push({ path: '/complate' });
+  };
+  const setCharacter = (image) => {
+    if (experience) {
+      experience.world.removeSticker();
+      experience.world.setCharacter(image.id);
+    }
+    selectedSticker.value = null;
+    selectedCharacter.value = image.id;
+  };
+
+  const setFrame = (image) => {
+    setFrameSrc.value = image.src;
+  };
+
+  const setSticker = (image) => {
+    playAudio();
+    experience.world.setSticker(image.name);
+    experience.world.removeCharacter();
+    selectedSticker.value = image.id;
+    selectedCharacter.value = false;
+  };
+
+  onMounted(() => {
+    console.log('Experience Mount!');
+
+    experience = new Experience(document.querySelector('.webgl'), saveImage);
+    console.log(experience);
+    // experience.value.init();
+  });
+  onUnmounted(() => {
+    console.log('Experience UnMount!');
+
+    // experience = new Experience(document.querySelector('.webgl'), saveImage);
+
+    // experience.init();
+  });
+
+  onBeforeRouteLeave(() => {
+    console.log('Experience Route Leave!');
+    experience.destroy();
+    experience.dispose();
+    experience.init();
+  });
+
+  return {
+    experience,
+    setCharacter,
+    setSticker,
+    setFrame,
+    saveImage,
+    experience,
+  };
+};
+
+export default useExperience;
